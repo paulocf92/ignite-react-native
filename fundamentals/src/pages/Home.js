f11ca77a91b2f871ef8e619f44499ff3112c7de8
Paulo Filho <pcf.feat@gmail.com>
2021-05-18 14:35:28 -0400

    chore: fleshing out layout with StyleSheet

--- fundamentals/src/pages/Home.js
+++ fundamentals/src/pages/Home.js
@@ -1,14 +1,67 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+  Text, 
+  StyleSheet, 
+  SafeAreaView, 
+  TextInput, 
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Home() {
   return (
     <>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Paulo Filho</Text>
-        <Text>1992</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Welcome, Paulo Filho</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="New skill"
+          placeholderTextColor="#999"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, { marginTop: 50 }]}>
+          My skills
+        </Text>
+      </SafeAreaView>
       
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1325',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
