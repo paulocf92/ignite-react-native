94d0d1c80a931e74331012146dd41854791bbd36
Paulo Filho <pcf.feat@gmail.com>
2021-05-18 14:08:01 -0400

    chore: draft one page

---
+++ fundamentals/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Paulo Filho</Text>
+        <Text>1992</Text>
+      </View>
+      
+    </>
+  );
+}
