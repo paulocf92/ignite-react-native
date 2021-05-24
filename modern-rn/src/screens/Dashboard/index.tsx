fa01d5b1cae2c906cca1e9d8dc3f80239bb38478
Paulo Filho <pcf.feat@gmail.com>
2021-05-24 18:03:47 -0400

    chore: drafting header keeping reponsive units

--- modern-rn/src/screens/Dashboard/index.tsx
+++ modern-rn/src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/6351070?v=4',
+              }}
+            />
+            <User>
+              <UserGreetings>Hi,</UserGreetings>
+              <UserName>Paulo</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
