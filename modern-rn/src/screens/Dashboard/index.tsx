a7a440fe0c9daa8461329fc778f311e90e60ebd8
Paulo Filho <pcf.feat@gmail.com>
2021-05-24 15:49:53 -0400

    chore: reorganize files and introduce styled components

---
+++ modern-rn/src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import {} from 'react-native';
+
+import { Container, Title } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
