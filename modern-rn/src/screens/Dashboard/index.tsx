fb002d4e6338e39a3009512f2a939450c901e07c
Paulo Filho <pcf.feat@gmail.com>
2021-05-25 20:28:33 -0400

    chore: draft highlight cards

--- modern-rn/src/screens/Dashboard/index.tsx
+++ modern-rn/src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { HighlightCard } from '../../components/HighlightCard';
+
 import {
   Container,
   Header,
@@ -32,6 +34,8 @@ export function Dashboard() {
           <Icon name='power' />
         </UserWrapper>
       </Header>
+
+      <HighlightCard />
     </Container>
   );
 }
