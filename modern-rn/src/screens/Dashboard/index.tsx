7bd2b91ef4815cea25613d19fc09927c023382c7
Paulo Filho <pcf.feat@gmail.com>
2021-05-25 20:11:19 -0400

    chore: add logout vector icon

--- modern-rn/src/screens/Dashboard/index.tsx
+++ modern-rn/src/screens/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import {
   User,
   UserGreetings,
   UserName,
+  Icon,
 } from './styles';
 
 export function Dashboard() {
@@ -27,6 +28,8 @@ export function Dashboard() {
               <UserName>Paulo</UserName>
             </User>
           </UserInfo>
+
+          <Icon name='power' />
         </UserWrapper>
       </Header>
     </Container>
