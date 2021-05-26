3507e0d9654b62a64f1c363e9d8bc1539a7197ff
Paulo Filho <pcf.feat@gmail.com>
2021-05-26 12:45:56 -0400

    feat: add Input component within Register screen

--- modern-rn/App.tsx
+++ modern-rn/App.tsx
@@ -3,7 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 import {
   useFonts,
@@ -25,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ modern-rn/src/screens/Register/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Input } from '../../components/Form/Input';
+
+import { Container, Header, Title, Form } from './styles';
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Input placeholder='Nome' />
+        <Input placeholder='Preço' />
+      </Form>
+    </Container>
+  );
+}
