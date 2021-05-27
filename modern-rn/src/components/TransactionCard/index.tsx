73353efee175feae952ec405342f36aaffebd3c1
Paulo Filho <pcf.feat@gmail.com>
2021-05-27 17:26:21 -0400

    feat: list transactions from async storage

--- modern-rn/src/components/TransactionCard/index.tsx
+++ modern-rn/src/components/TransactionCard/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { categories } from '../../utils/categories';
+
 import {
   Container,
   Title,
@@ -10,16 +13,11 @@ import {
   Date,
 } from './styles';
 
-interface Category {
-  name: string;
-  icon: string;
-}
-
 export interface TransactionCardProps {
   type: 'positive' | 'negative';
-  title: string;
+  name: string;
   amount: string;
-  category: Category;
+  category: string;
   date: string;
 }
 
@@ -28,9 +26,11 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+  const [category] = categories.filter(item => item.key === data.category);
+
   return (
     <Container>
-      <Title>{data.title}</Title>
+      <Title>{data.name}</Title>
 
       <Amount type={data.type}>
         {data.type === 'negative' && '- '}
@@ -39,8 +39,8 @@ export function TransactionCard({ data }: Props) {
 
       <Footer>
         <Category>
-          <Icon name={data.category.icon} />
-          <CategoryName>{data.category.name}</CategoryName>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
         </Category>
 
         <Date>{data.date}</Date>
