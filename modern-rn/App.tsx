83d2c5c05a0d260da11fac23261d244a3b097530
Paulo Filho <pcf.feat@gmail.com>
2021-05-27 11:30:56 -0400

    feat: draft basic tabs navigation

--- modern-rn/App.tsx
+++ modern-rn/App.tsx
@@ -4,8 +4,10 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 import {
   useFonts,
@@ -27,7 +29,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
