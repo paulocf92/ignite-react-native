82308caced1e9f28d0a0101c2b019fff9cc15282
Paulo Filho <pcf.feat@gmail.com>
2021-05-27 10:46:16 -0400

    feat: add react hook form to handle form data

--- modern-rn/src/screens/Register/index.tsx
+++ modern-rn/src/screens/Register/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Modal } from 'react-native';
+import { useForm } from 'react-hook-form';
 
-import { Input } from '../../components/Form/Input';
+import { InputForm } from '../../components/Form/InputForm';
 import { Button } from '../../components/Form/Button';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
@@ -16,6 +17,11 @@ import {
   TransactionTypes,
 } from './styles';
 
+interface FormData {
+  name: string;
+  amount: string;
+}
+
 export function Register() {
   const [category, setCategory] = useState({
     key: 'category',
@@ -23,6 +29,8 @@ export function Register() {
   });
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const { control, handleSubmit } = useForm();
 
   function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
@@ -36,6 +44,17 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    };
+
+    console.log(data);
+  }
+
   return (
     <Container>
       <Header>
@@ -44,8 +63,8 @@ export function Register() {
 
       <Form>
         <Fields>
-          <Input placeholder='Nome' />
-          <Input placeholder='Preço' />
+          <InputForm name='name' control={control} placeholder='Nome' />
+          <InputForm name='amount' control={control} placeholder='Preço' />
 
           <TransactionTypes>
             <TransactionTypeButton
@@ -68,7 +87,7 @@ export function Register() {
           />
         </Fields>
 
-        <Button title='Enviar' />
+        <Button title='Enviar' onPress={handleSubmit(handleRegister)} />
       </Form>
 
       <Modal statusBarTranslucent visible={categoryModalOpen}>
