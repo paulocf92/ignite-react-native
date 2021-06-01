aac786a6d77a6c4fd1b9ce8d2b224775ce46cc1d
Paulo Filho <pcf.feat@gmail.com>
2021-06-01 19:58:16 -0400

    chore: add more elements to car details screen

--- api-animations/src/screens/CarDetails/index.tsx
+++ api-animations/src/screens/CarDetails/index.tsx
@@ -4,7 +4,20 @@ import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -26,6 +39,26 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 }
