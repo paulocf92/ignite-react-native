138ffd30c5a7e53252304fedd67beaa2b7199214
Paulo Filho <pcf.feat@gmail.com>
2021-05-31 20:20:40 -0400

    chore: draft header, import assets

--- api-animations/src/screens/Home/index.tsx
+++ api-animations/src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
--- api-animations/src/screens/Home/styles.ts
+++ api-animations/src/screens/Home/styles.ts
@@ -1,14 +1,28 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-
-  justify-content: center;
-  align-items: center;
-
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
