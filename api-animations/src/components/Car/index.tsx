5abcf16e4387fc187594c15db03b6efb27007586
Paulo Filho <pcf.feat@gmail.com>
2021-05-31 21:11:08 -0400

    feat: add car cards

---
+++ api-animations/src/components/Car/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import GasolineSvg from '../../assets/gasoline.svg';
+
+import {
+  Container,
+  Details,
+  Brand,
+  Name,
+  About,
+  Rent,
+  Period,
+  Price,
+  Type,
+  CarImage,
+} from './styles';
+
+interface CarData {
+  brand: string;
+  name: string;
+  rent: {
+    period: string;
+    price: number;
+  };
+  thumbnail: string;
+}
+
+interface Props {
+  data: CarData;
+}
+
+export function Car({ data }: Props) {
+  return (
+    <Container>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>{`R$ ${data.rent.price}`}</Price>
+          </Rent>
+
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+      </Details>
+
+      <CarImage
+        source={{
+          uri: data.thumbnail,
+        }}
+        resizeMode='contain'
+      />
+    </Container>
+  );
+}
--- api-animations/src/screens/Home/index.tsx
+++ api-animations/src/screens/Home/index.tsx
@@ -4,9 +4,32 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 export function Home() {
+  const carDataOne = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail:
+      'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -20,6 +43,9 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   );
 }
