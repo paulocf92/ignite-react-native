80f914d3f1ebb0e4d2b5ae861d1819a86b116972
Paulo Filho <pcf.feat@gmail.com>
2021-06-02 20:27:47 -0400

    fix(style): change status bar to light content in the Scheduling Complete component

--- api-animations/src/screens/SchedulingComplete/index.tsx
+++ api-animations/src/screens/SchedulingComplete/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useWindowDimensions } from 'react-native';
+import { StatusBar, useWindowDimensions } from 'react-native';
 
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
@@ -13,6 +13,11 @@ export function SchedulingComplete() {
 
   return (
     <Container>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       <LogoSvg width={width} />
 
       <Content>
