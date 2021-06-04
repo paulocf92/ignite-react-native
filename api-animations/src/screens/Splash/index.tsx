71c735b33cdde0ed3b1ae051674d69c4b260c1aa
Paulo Filho <pcf.feat@gmail.com>
2021-06-04 19:50:40 -0400

    chore: add splash screen

--- api-animations/src/screens/Splash/index.tsx
+++ api-animations/src/screens/Splash/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { Button, Dimensions, StatusBar, StyleSheet } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
+const width = Dimensions.get('window').width;
+
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bounce,
+          }),
+        },
+      ],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * (width - 100);
+  }
+
   return (
     <Container>
       <StatusBar
@@ -11,6 +38,18 @@ export function Splash() {
         backgroundColor='transparent'
         translucent
       />
+
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button title='Mover' onPress={handleAnimationPosition} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
--- api-animations/src/screens/Splash/styles.ts
+++ api-animations/src/screens/Splash/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.header};
 `;
