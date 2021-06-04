794893fe7e0bd5c38a9bc251269d2421fcc85312
Paulo Filho <pcf.feat@gmail.com>
2021-06-04 18:09:54 -0400

    fix: fix cars quantity in Home

--- api-animations/src/screens/Home/index.tsx
+++ api-animations/src/screens/Home/index.tsx
@@ -61,7 +61,7 @@ export function Home() {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de 12 carros</TotalCars>
+          <TotalCars>Total de {cars.length} carros</TotalCars>
         </HeaderContent>
       </Header>
 
