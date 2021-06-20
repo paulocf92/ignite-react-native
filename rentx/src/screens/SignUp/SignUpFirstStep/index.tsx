6a918b3753236bf15a79c053e1d312dfaa2bd706
Paulo Filho <pcf.feat@gmail.com>
2021-06-20 15:29:39 -0400

    chore: improve first step accessibility

--- rentx/src/screens/SignUp/SignUpFirstStep/index.tsx
+++ rentx/src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
 import {
   Container,
@@ -22,21 +29,31 @@ export function SignUpFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleGoBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleGoBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
 
-      <Title>Crie sua{'\n'}conta</Title>
-      <SubTitle>Faça seu cadastro de{'\n'}forma rápida e fácil</SubTitle>
+          <Title>Crie sua{'\n'}conta</Title>
+          <SubTitle>Faça seu cadastro de{'\n'}forma rápida e fácil</SubTitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+
+            <Input iconName='user' placeholder='Nome' />
+            <Input iconName='mail' placeholder='E-mail' />
+            <Input iconName='credit-card' placeholder='CNH' />
+          </Form>
+
+          <Button title='Próximo' />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- rentx/src/screens/SignUp/SignUpFirstStep/styles.ts
+++ rentx/src/screens/SignUp/SignUpFirstStep/styles.ts
@@ -3,7 +3,6 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
@@ -51,4 +50,6 @@ export const FormTitle = styled.Text`
   font-family: ${({ theme }) => theme.fonts.secondary_600};
   color: ${({ theme }) => theme.colors.title};
   font-size: ${RFValue(20)}px;
+
+  margin-bottom: 24px;
 `;
